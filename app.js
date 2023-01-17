ca2ebad0e86e5537047090a73d888594636e6483
SalmaanMushtaq <salmaan@peko.one>
2023-01-17 17:47:09 +0530

    Added the database and adding dynamic functionality of List route and add route

--- app.js
+++ app.js
@@ -3,15 +3,30 @@ const app = express();
 const path = require('path');
 const ejsMate = require('ejs-mate');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://localhost:27017/gameDB')
-//     .then(() => {
-//         console.log("CONNECTION OPEN!!!")
-//     })
-//     .catch(err => {
-//         console.log("OH NO ERROR!!!");
-//         console.log(err)
-//     })
+mongoose.set('strictQuery', true)
+
+mongoose.connect("mongodb+srv://khanSalmaan:Salmaan786@newcluster.fzkg6ri.mongodb.net/test")
+    .then(() => {
+        console.log("Data Base Connected");
+    })
+    .catch(err => {
+        console.log("Oh!! Database did not connect");
+        console.log(err);
+    })
+
+const gameSchema = new mongoose.Schema({
+    title: String,
+    creator: String,
+    width: Number,
+    height: Number,
+    fileName: String,
+    thumbnailFile: String
+});
+
+const Game = mongoose.model("Game", gameSchema);
+
 app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,33 +37,6 @@ app.get('/', (req, res) => {
     res.render('components/home');
 });
 
-const gamesData = [
-    {
-        title: "Learn to Fly 2",
-        creator: "light_bringer777",
-        width: 640,
-        height: 480,
-        fileName: "learntofly2.swf",
-        thumbnailFile: "Learn_To_Fly_2.jpg"
-    },
-    {
-        title: "Run 3",
-        creator: "player_03",
-        width: 800,
-        height: 600,
-        fileName: "run3.swf",
-        thumbnailFile: "run3.jpg"
-    },
-    {
-        title: "Continuity",
-        creator: "glimajr",
-        width: 640,
-        height: 480,
-        fileName: "continuity.swf",
-        thumbnailFile: "booty.png"
-    }
-]
-
 app.get('/game/:title/:creator/:width/:height/:fileName/:thumbnailFile', (req, res) => {
     const title = req.params.title;
     const creator = req.params.creator;
@@ -57,13 +45,19 @@ app.get('/game/:title/:creator/:width/:height/:fileName/:thumbnailFile', (req, r
     const fileName = req.params.fileName;
     const thumbnailFile = req.params.thumbnailFile;
     res.render('components/game', { title, creator, width, height, fileName, thumbnailFile });
-})
+});
 
 
 //Route to see all the gamesList
 app.get('/list', (req, res) => {
     //game list
-    res.render('components/list', { gamesData });
+    Game.find({}, (error, games) => {
+        if (error) {
+            console.log("Problem in retreiving the data from the database")
+        } else {
+            res.render('components/list', { gamesData: games });
+        }
+    });
 })
 
 
@@ -73,10 +67,24 @@ app.get('/addgame', (req, res) => {
 
 app.post('/addgame', (req, res) => {
     const data = req.body;
-    gamesData.push(data);
-    res.redirect('list');
+    Game.create({
+        title: data.title,
+        creator: data.creator,
+        width: data.width,
+        height: data.height,
+        fileName: data.fileName,
+        thumbnailFile: data.thumbnailFile
+    }, (error, data) => {
+        if (error) {
+            console.log("Data cannot be added in the database");
+        } else {
+            console.log("Data successfully added iin the database!!");
+            console.log(data)
+        }
+    });
+    res.redirect('/list');
 })
 
 app.listen(3000, () => {
-    console.log('Gamming Website has started on port 3000')
+    console.log('Gamming website is listening on port 3000')
 });
